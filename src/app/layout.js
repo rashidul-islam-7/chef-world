f1f3224e23918decbe3588d724fe53e5cd21e835
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-07-25 10:27:01 +0600

    fixing build error

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,8 @@ import NavBar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { ToastContainer } from "react-toastify";
 import { SessionProvider } from "@/components/Provider/SessionProvider";
-// import { auth } from "@/lib/auth";
-// import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
